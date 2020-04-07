2c8e823ce53724a5815232cab73c842830c5608e
Akash Kumar <aksh.iem22@gmail.com>
2020-04-08 00:43:02 +0530

    structured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import { Cards, Charts, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
 class App extends React.Component {
+
+   async componentDidMount() {
+        const data = await fetchData();
+
+        console.log(data);
+    }
+
     render() {
         return(
-            <div>
-                <h1>
-                    Hey I created React Page
-                </h1>
+            <div className={ styles.container }>
+                <Cards />
+                <CountryPicker />
+                <Charts />
             </div>
         )
     }
---
+++ src/components/Charts/Charts.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Charts = () => {
+    return (
+        <h1>Charts section</h1>
+    )
+}
+
+export default Charts;
