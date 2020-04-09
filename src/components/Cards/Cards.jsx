d79db6e2fc6461b30d72eadff8eefdc40f97408f
Akash Kumar <aksh.iem22@gmail.com>
2020-04-09 16:34:03 +0530

    api added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
+import styles from './Cards.module.css';
 
-const Cards = () => {
+const Cards = ({data: { confirmed, recovered, deaths, lastUpdate }}) => {
+    if(!confirmed) {
+        return "Loading...";
+    }
+    
     return (
-        <h1>Card section</h1>
+        <div className={ styles.container } >
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5"> 
+                            <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+                         </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2">Number of Active Cases</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5"> Real Data </Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of Active Cases</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5"> Real Data </Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant="body2">Number of Active Cases</Typography>
+                    </CardContent>
+                </Grid>
+
+            </Grid>
+        </div>
     )
 }
 
