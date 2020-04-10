de27deb702182b544c4a5f4f7e747f3379f9d8ae
Akash Kumar <aksh.iem22@gmail.com>
2020-04-10 15:23:58 +0530

    fix minor issues

--- src/components/CountyPicker/CountryPicker.jsx
+++ src/components/CountyPicker/CountryPicker.jsx
@@ -20,14 +20,14 @@ const CountryPicker = ({ handleCountryChange }) => {
     // console.log(fetchedCountries);
 
     return (
-        <div className={styles.container}>
+        
             <FormControl className={ styles.formControl }>
                 <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-                    <option value="global">Global</option>
+                    <option value="">Global</option>
             {fetchedCountries.map((country, i)=> <option key={i} value={country} >{country}</option>)}
                 </NativeSelect>
             </FormControl>
-        </div>
+     
     )
 }
 
