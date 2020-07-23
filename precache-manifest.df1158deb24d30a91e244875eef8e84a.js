2a70e578d0b5fcb18fcf7c6e5988fbd6cb7638ae
Akash Kumar <aksh.iem22@gmail.com>
2020-07-23 20:40:02 +0530

    Updates

---
+++ precache-manifest.df1158deb24d30a91e244875eef8e84a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30de86bd5943d7d0f35fcbe071277c9b",
+    "url": "/Covid-19-tracker/index.html"
+  },
+  {
+    "revision": "844ad9e1e7c9bf67f759",
+    "url": "/Covid-19-tracker/static/css/main.fab662a4.chunk.css"
+  },
+  {
+    "revision": "45501542410eef45384e",
+    "url": "/Covid-19-tracker/static/js/2.65a9f19b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-tracker/static/js/2.65a9f19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844ad9e1e7c9bf67f759",
+    "url": "/Covid-19-tracker/static/js/main.89cd0aff.chunk.js"
+  },
+  {
+    "revision": "579bb4d6337630b9f780",
+    "url": "/Covid-19-tracker/static/js/runtime-main.384237b2.js"
+  }
+]);
