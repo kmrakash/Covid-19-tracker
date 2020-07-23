a4e37244d5817c99da65ea2913ff85882e808d2a
Akash Kumar <aksh.iem22@gmail.com>
2020-07-23 21:12:38 +0530

    Minor changes and added README

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import { Cards, Charts, CountryPicker } from './components';
-import styles from './App.module.css';
-import { fetchData } from './api';
+import React from "react";
+import { Cards, Charts, CountryPicker } from "./components";
+import Footer from "./components/Footer";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
 
-    state = {
-        data: {},
-        country: ''
-    }
+  // fetch data from api
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    console.log(fetchedData); // for debugging
+    this.setState({
+      data: fetchedData,
+    });
+  }
 
-    // fetch data from api
-   async componentDidMount() {
-        const fetchedData = await fetchData();
-        console.log(fetchedData); // for debugging 
-        this.setState({
-            data: fetchedData,
-        });
-    }
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
 
-    handleCountryChange = async (country) => {
-        
-        const fetchedData = await fetchData(country);
+    this.setState({
+      data: fetchedData,
+      country: country,
+    });
+  };
 
-        this.setState({
-            data: fetchedData,
-            country:country
-        })
-    }
+  render() {
+    // a better way to send props
+    const { data, country } = this.state;
 
-    render() {
-        // a better way to send props
-        const { data, country } = this.state;
-
-        return(
-            <div className={ styles.container }>
-                <Cards data={ data } />
-                <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Charts data={ data } country={ country } />
-            </div>
-        )
-    }
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Charts data={data} country={country} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+export default () => (
+  <Grid alignContent="center">
+    <h3>
+      Built with{" "}
+      <span role="img" aria-label="Love emoji">
+        ❤️
+      </span>{" "}
+      by <a href="https://github.com/kmrakash">Akash </a>{" "}
+    </h3>
+  </Grid>
+);
